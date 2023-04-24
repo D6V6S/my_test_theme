bd0d44a7fe6d7250504a3bfc7b2b194f0ac5d66b
D6V6S <96797621+D6V6S@users.noreply.github.com>
2023-04-24 15:52:03 +0300

    Page tamplate

---
+++ page-templates/post.php
@@ -0,0 +1,6 @@
+<?php
+/*
+* Template name: mytheme_Post test
+* Template post type: post
+*/
+?><h1>Hi gys</h1>
---
+++ page-templates/team.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* Template name: mytheme_Team
+*/
+?>
+
+<?php get_header();?>
+
+<?php get_template_part('template-parts/breadcrumbs'); ?>
+
+<h1>Hi gys</h1>
+
+
+
+<?php get_footer(); ?>
